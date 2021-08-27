117a584d3443b05c013fd8c56341d79eac71b89f
abdellatif-dev <abdellatif.devlog@gmail.com>
2021-08-27 02:47:27 +0100

    runtime

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const today = document.getElementById("today");
-const search = document.getElementById("search")
-const btn = document.getElementById("btn")
+const search = document.getElementById("search");
+const btn = document.getElementById("btn");
 const d = new Date();
 
-today.textContent = d.toUTCString()
+const engine = "search.brave.com";
+today.textContent = d.toUTCString();
 
 function beBrave() {
-    window.location = "https://search.brave.com/search?q=" + search.value
+    window.location = "https://" + engine + "/search?q=" + search.value;
 }
 
 btn.addEventListener('click', beBrave);
